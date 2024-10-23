737da45a5ae47bd10d168a5a7212ee411f54c965
synksta <contriturbamentum@gmail.com>
2024-10-23 17:57:55 +0700

    feat(post): update post model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,20 +4,34 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
 
 class Post extends Model
 {
+
+    use HasSlug;
 
     protected $fillable = ['title', 'description', 'content', 'thumbnail', 'category_id'];
 
     public function tags()
     {
-        return $this->belongsToMany(Tag::class);
+        return $this->belongsToMany(Tag::class)->withTimestamps();
     }
 
     public function category()
     {
         return $this->belongsTo(Category::class);
+    }
+
+    /**
+     * Get the options for generating the slug.
+     */
+    public function getSlugOptions(): SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('title')
+            ->saveSlugsTo('slug');
     }
 
     use HasFactory;
