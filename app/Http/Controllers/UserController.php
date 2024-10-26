6c7304c7d9380ac99da4cfdf74437b7be03537cb
synksta <contriturbamentum@gmail.com>
2024-10-26 17:03:20 +0700

    feat(reguser): created request, added basic methods to controller, created blade for registration

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreUser;
 use Illuminate\Http\Request;
 
-class UserController extends Controller {}
+class UserController extends Controller
+{
+    public function create()
+    {
+        return view('user.create');
+    }
+
+    public function store(StoreUser $request)
+    {
+        dd($request->all());
+    }
+}
