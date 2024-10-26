19e6b5b4c112eb71f19e4188ed7575bfef3d4201
synksta <contriturbamentum@gmail.com>
2024-10-26 16:52:19 +0700

    feat(post): optimized queries on /posts, created new controller and routes for user registration, added laravel-debugbar

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\MainController;
 use App\Http\Controllers\Admin\PostController;
@@ -17,8 +18,11 @@ Route::group(['prefix' => 'admin', 'namespace' => 'App\Http\Controllers\Admin'],
     Route::resource('/categories', CategoryController::class);
     Route::resource('/tags', TagController::class);
     Route::resource('/posts', PostController::class);
-    // 
+
 
     // Route::get('/', 'MainController@index')->name('admin.index');
 
 });
+
+Route::get('/register', [UserController::class, 'create'])->name('register.create');
+Route::post('/register', [UserController::class, 'store'])->name('register.store');
